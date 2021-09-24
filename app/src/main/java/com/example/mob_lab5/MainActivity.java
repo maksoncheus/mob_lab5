20a3a2bb340d4d33fcd52d3639d25d4882a4807c
maksoncheus <maksoncheus@gmail.com>
2021-09-24 23:35:49 +0700

    загрузчик видео в плеер, добавление медиа-контроллера

--- app/src/main/java/com/example/mob_lab5/MainActivity.java
+++ app/src/main/java/com/example/mob_lab5/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mob_lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        VideoView videoView =(VideoView)findViewById(R.id.videoView);
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video1);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
     }
 }
